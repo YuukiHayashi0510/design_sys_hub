d5f058c64ab4c93bb3f43287d897ac428b8f6573
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2023-08-15 16:52:26 +0900

    fix: unused func

--- src/pages/api/post/[id]/star/service.ts
+++ src/pages/api/post/[id]/star/service.ts
@@ -8,14 +8,3 @@ export async function createStar(data: CreateStarData) {
 export async function deleteStar(id: string) {
   return await prisma.star.delete({ where: { id } })
 }
-
-// private
-type HasStarData = CreateStarData
-
-const exists = async (data: HasStarData) => {
-  const starCount = await prisma.star.count({
-    where: { postId: data.postId, userId: data.userId },
-  })
-
-  return starCount === 1
-}
