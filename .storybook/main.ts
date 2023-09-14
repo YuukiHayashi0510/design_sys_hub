f7a1918b265996e0193603046cf26648666de3ca
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2023-09-14 18:21:43 +0900

    fix: staticDirs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -24,7 +24,6 @@ const config: StorybookConfig = {
   docs: {
     autodocs: 'tag',
   },
-  staticDirs: ['../public'],
   typescript: {
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
